7f21f1938cbd5d1892a0f799e5f6178244eea043
Steve Calderon <steve@fastly.com>
2019-02-28 18:20:47 -0500

    wip

--- src/App.js
+++ src/App.js
@@ -1,3 +1,34 @@
 import React from 'react';
+import SpotifyButton from './SpotifyConnector';
 
 const AppHeader = () => <h1 className="app-header">Rotache</h1>;
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render() {
+        let initialView = this.state.spotifyAuth ? (
+            <p>Authenticated to spotify</p>
+        ) : (
+            <SpotifyButton 
+                app = {this}
+            />
+        )        
+
+        return(
+            <div>
+                <AppHeader />
+                {initialView}
+            </div>
+
+        );
+    }
+}
+
+
+export default App;
